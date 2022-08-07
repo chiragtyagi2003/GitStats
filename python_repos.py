f3ff2abb564d7f15de92532cb5a7623be104304c
chiragtyagi <tyagichirag06@gmail.com>
2022-08-07 16:54:05 +0530

    Access Info about each Repository

--- python_repos.py
+++ python_repos.py
@@ -13,6 +13,22 @@ print(f"Status code: {r.status_code}")
 #store the API response in a variable
 response_dict = r.json()
 
-#process the results
-#keys of the response dict
-print(response_dict.keys())
+print(f"Total Repositories: {response_dict['total_count']}")
+
+#info about repositories
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+#selected info about the each repo
+print("\nInformation About Each Repository:")
+print("\n")
+for repo_dict in repo_dicts:
+    
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
+    print("\n")
